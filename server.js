0b1756489610bbab8f656c3ea93ee0e421913bfa
_d6mi_ <domagoj.kulundzic@newscron.com>
2015-11-03 10:27:57 +0100

    Refactor API with underscore.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore')
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -18,32 +19,34 @@ app.get('/todos', function (req, res) {
 });
 
 app.get('/todos/:id', function (req, res) {
-	var todoId = parseInt(req.params.id, 10)	;
+	var todoId = parseInt(req.params.id, 10);
 	console.log(todoId);
 
-	var matchedTodo;
-
-	todos.forEach(function (todo) {
-		if(todoId === todo.id) {
-			matchedTodo = todo;
-		}
-	});
+	var matchedTodo = _.findWhere(todos, { id: todoId});
 
 	if(matchedTodo) {
 		res.json(matchedTodo);
 	} else {
-		res.status(404).send();
+		res.status(404).json({
+			error: 'Invalid params'
+		});
 	}
 });
 
 app.post('/todos', function (req, res) {
-	var body = req.body;
+	var todo = _.pick(req.body, 'description', 'completed');
 
-	body.id = todoNextId++;
+	console.log(todo);
 
-	todos.push(body);
-	
-	res.json(body);
+	if(!_.isBoolean(todo.completed) || !_.isString(todo.description) || todo.description.trim().length == 0) {
+		return res.status(400).send();
+	}
+
+	todo.description = todo.description.trim();
+	todo.id = todoNextId++;
+	todos.push(todo);
+
+	res.json(todo);
 });
 
 app.listen(PORT, function () {
